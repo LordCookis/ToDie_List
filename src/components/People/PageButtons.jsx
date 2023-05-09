db371a01b8e5a3836c6d5638a5c876397f3dcb7b
_RynnLee <38906839+Rynn-Lee@users.noreply.github.com>
2023-05-09 23:53:39 +0600

    Minor changes / clean up
    - some styles
    - clean up

--- src/components/People/PageButtons.jsx
+++ src/components/People/PageButtons.jsx
@@ -1,9 +1,11 @@
-export const PageButtons = ({page, setPage, people}) => {
+import { PageList } from "./PageList"
+
+export const PageButtons = ({page, setPage, people, total}) => {
   return(
     <center>
     <div id="div-page">
       <button id="page-button" onClick={()=>(page > 0) && setPage(page-1)}>Назад</button>
-      <span id="page-span">{page + 1} / {Math.ceil(people.length / 10)}</span>
+      <PageList total={total} setPage={setPage} page={page}/>
       <button id="page-button" onClick={()=>((page + 1) < (people.length / 10)) && setPage(page+1)}>Далее</button>
     </div>
   </center>
